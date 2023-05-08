806b46d7abf6c3b62fd7674b34a1540c6364f2cb
zilconia <85538741+zilconia@users.noreply.github.com>
2023-05-08 09:24:40 +0900

    メモ書き

--- Py-Linebot/index.py
+++ Py-Linebot/index.py
@@ -15,23 +15,28 @@ handler = WebhookHandler(SECRET)
 # Webhookからのリクエストを受信するためのエンドポイントを作成します
 @app.route("/callback", methods=["POST"])
 def callback():
+    # リクエストの署名確認
     signature = request.headers["X-Line-Signature"]
+    # Lineアプリ側のメッセージを取得（str型として取得）
     body = request.get_data(as_text=True)
+
     app.logger.info("Request body: " + body)
 
+    # リクエストによるアプリの動作結果を返す
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return "OK"
 
 
 # MessageEventの場合の処理を実行する関数を定義します
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # 送信元のメッセージに返信する機能
     line_bot_api.reply_message(
         event.reply_token,
+        # メッセージを送信（直前のメッセージをそのまま送信）
         TextSendMessage(text=event.message.text)
     )
 
