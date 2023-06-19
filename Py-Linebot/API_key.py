0656664716f4e00aae1a495fea7cd9ade8b4b02b
zilconia <85538741+zilconia@users.noreply.github.com>
2023-06-19 11:51:24 +0900

    不要ファイル削除

--- Py-Linebot/API_key.py
+++ Py-Linebot/API_key.py
@@ -1,6 +1,6 @@
 # チャネルアクセストークン
-Token="pyVEnT8mvOP5vxarGh3879qwCpOs5GLXGjzLsw5wkdVIZYLg3B0AIpK8aDFmWWfNKX+vxLOTmzlnf1J3rj76C4nFQRWc0DPKvBY90xJFvO0MsDRfMVqnLhIBasjigqmO9gqhGb+pblkJYonXH82voAdB04t89/1O/w1cDnyilFU="
+Token="Line-Tokens"
 # チャネルシークレット
-Secret="2b242b648b0a9bdea6716f0547665ae6"
+Secret="Line-Secrets"
 # ChatGPT のアクセストークン
-GPT_key="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"
+GPT_key="GPT-keys"
